19bf18812b66427b9496b257287ce2c0ec0820a0
Erika Matesanz <erikamatesanz@gmail.com>
2019-03-06 21:49:27 +0100

    typescript and css improved

--- src/Card/index.tsx
+++ src/Card/index.tsx
@@ -6,22 +6,48 @@ import './Card.scss';
 interface CardProps {
   id: number,
   name: string,
-  onClick: Function
 }
 
-const Card = (props: CardProps) => {
+interface CardState {
+  isOpen: boolean
+}
 
-  const { id, name, onClick } = props;
+class Card extends Component<CardProps, CardState> {
+  state = {
+    isOpen: false,
+  }
 
-  return (
-    <article className="card" onClick={()=>{onClick(id)}}>
-      <span className="card--left"> { name } </span>
-      <p>Scrum poker</p>
-      <p className="card__name"> {name} </p>
-      <span className="card--right"> { name } </span>
-    </article>
-  )
+  openCard = () => {
+    const { isOpen } = this.state
+    if (isOpen === false) this.setState({ isOpen: true });
+  }
 
+
+  closeCard = () => {
+    const { isOpen } = this.state
+    this.setState({ isOpen: !isOpen });
+  }
+
+  render () {
+    const { id, name } = this.props
+    const { isOpen } = this.state
+    let className = 'card'
+    let closeBtn
+    if (isOpen) {
+      className = `${className} ${className}--is-open`
+      closeBtn = `${<span>X</span>}`
+    }
+
+    return (
+      <article className={className} onClick={this.openCard}>
+        <span className="card--left"> { name } </span>
+        <p>Scrum poker</p>
+        <p className="card__name"> {name} </p>
+        <span className="card--right"> { name } </span>
+        { isOpen && <span className="card__close" onClick={this.closeCard}>X</span> }
+      </article>
+    )
+  }
 }
 
 export default Card;
--- src/Cards/index.tsx
+++ src/Cards/index.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import Card from '../Card'
 import Settings from '../Settings'
-import PopUp from '../PopUp'
 import './Cards.scss';
 
 interface CardsProps {
@@ -10,19 +9,13 @@ interface CardsProps {
 }
 
 interface CardsState {
-  showPopup: boolean,
   id: number
 }
 
 class Cards extends Component<CardsProps, CardsState> {
   state = {
-    showPopup: false,
     id: 0
   }
-
-  showCard = (id: number) => this.setState({ showPopup: true, id });
-
-  togglePopup = () => this.setState({ showPopup: !this.state.showPopup });
   
   render () {
     const { cards } = this.props;
@@ -30,10 +23,8 @@ class Cards extends Component<CardsProps, CardsState> {
     const hasCountDown = true;
     return (
       <section className="wrapper">
-        { cards.map((card, idx) => <Card key={idx} id={idx} name={card} onClick={this.showCard} />) }
+        { cards.map((card, idx) => <Card key={idx} id={idx} name={card} />) }
         <Settings/>
-
-        { this.state.showPopup && <PopUp text={cards[id]} hasCountDown={hasCountDown} closePopup={this.togglePopup} /> }
       </section>
     )
   }
